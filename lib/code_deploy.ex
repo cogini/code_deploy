1f5e5b86ca9cd54eb2aa1109e005dda51bef0b7d
Jake Morrison <jake@cogini.com>
2018-08-17 07:25:01 -0700

    Add template for systemd service

--- lib/code_deploy.ex
+++ lib/code_deploy.ex
@@ -2,17 +2,78 @@ defmodule CodeDeploy do
   @moduledoc """
   Documentation for CodeDeploy.
   """
+  @app :code_deploy
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CodeDeploy.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def config do
+    config(Mix.Project.config())
   end
+
+  def config(project_config) do
+    app = to_string(project_config[:app])
+    config = project_config[:systemd] || []
+    config = [app: app] ++ config
+    service_name = config[:service_name] || String.replace(app, "_", "-")
+    app_user = config[:app_user] || app
+
+    [
+      app: app,
+      service_name: service_name,
+      app_user: app_user,
+      app_group: config[:app_group] || app_user,
+      org: config[:org],
+      deploy_dir: config[:deploy_dir] || deploy_dir(config),
+      mix_env: config[:mix_env] || Mix.env(),
+      release_mutable_dir: config[:release_mutable_dir] || "/run/#{service_name}",
+      env_lang: config[:env_lang] || "en_US.UTF-8",
+      env_port: config[:env_port] || 4000,
+      conform_conf_path: config[:conform_conf_path],
+      limit_nofile: config[:limit_nofile] || 65535,
+      umask: config[:umask] || "0027",
+      restart_sec: config[:restart_sec] || 5,
+      runtime_directory: config[:runtime_directory] || service_name,
+      runtime_directory_mode: config[:runtime_directory_mode] || "0750",
+      runtime_directory_preserve: config[:runtime_directory_preserve] || "no",
+      configuration_directory: config[:configuration_directory] || service_name,
+      configuration_directory_mode: config[:configuration_directory_mode] || "0750",
+      logs_directory: config[:logs_directory] || service_name,
+      logs_directory_mode: config[:logs_directory_mode] || "0750",
+      state_directory: config[:state_directory] || service_name,
+      state_directory_mode: config[:state_directory_mode] || "0750",
+      chroot: config[:chroot],
+      root_directory: config[:root_directory],
+      read_write_paths: config[:read_write_paths] || [],
+      read_only_paths: config[:read_only_paths] || [],
+      inaccessible_paths: config[:inaccessible_paths] || [],
+      systemd_version: config[:systemd_version] || 235,
+      paranoia: config[:paranoia],
+    ]
+
+  end
+
+  def template(name, params \\ []) do
+    Application.app_dir(@app, Path.join("priv", "templates"))
+    |> Path.join("#{name}.eex")
+    |> template_path(params)
+  end
+
+  @spec template_path(String.t(), Keyword.t()) :: {:ok, String.t()} | {:error, term}
+  def template_path(template_path, params \\ []) do
+    {:ok, EEx.eval_file(template_path, params, [trim: true])}
+  rescue
+    e ->
+      {:error, {:template, e}}
+  end
+
+  def deploy_dir(config) do
+    app = config[:app]
+    service_name = config[:service_name] || String.replace(app, "_", "-")
+    base_dir = config[:base_dir] || "/srv"
+    case Keyword.fetch(config, :org) do
+      {:ok, org} ->
+        Path.join([base_dir, org, service_name])
+      :error ->
+        Path.join([base_dir, service_name])
+      end
+  end
+
 end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule CodeDeploy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      systemd: systemd()
+    ]
+  end
+
+  def systemd do
+    [
+      chroot: true,
+      paranoia: true,
     ]
   end
 
@@ -21,6 +29,7 @@ defmodule CodeDeploy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:distillery, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
